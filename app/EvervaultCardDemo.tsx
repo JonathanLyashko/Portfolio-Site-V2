70e838a2ab4a83a4bb1dd91c0040fa2388a1673d
Jonathan Lyashko <lyashko.jonathan@gmail.com>
2024-03-12 01:06:49 +0200

    Completely remove dark mode.

--- app/EvervaultCardDemo.tsx
+++ app/EvervaultCardDemo.tsx
@@ -15,50 +15,50 @@ export function EvervaultCardDemo() {
         
 
         <div className="flex justify-around flex-wrap gap-8">
-            <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm w-80 mx-auto p-4 relative h-[25rem]">
-                <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+            <div className="border border-black/[0.2] flex flex-col items-start max-w-sm w-80 mx-auto p-4 relative h-[25rem]">
+                <Icon className="absolute h-6 w-6 -top-3 -left-3  text-black" />
+                <Icon className="absolute h-6 w-6 -bottom-3 -left-3  text-black" />
+                <Icon className="absolute h-6 w-6 -top-3 -right-3  text-black" />
+                <Icon className="absolute h-6 w-6 -bottom-3 -right-3  text-black" />
 
                 <EvervaultCard text="Fullstack Development" />
 
-                <h2 className="dark:text-white text-black mt-4 text-sm font-light">
+                <h2 className=" text-black mt-4 text-sm font-light">
                     Building applications accessible by anyone, anywhere.
                 </h2>
-                <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
+                <p className="text-sm border font-light border-black/[0.2] rounded-full mt-4 text-black  px-2 py-0.5">
                     1
                 </p>
             </div>
 
-            <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm w-80 mx-auto p-4 relative h-[25rem]">
-                <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+            <div className="border border-black/[0.2] flex flex-col items-start max-w-sm w-80 mx-auto p-4 relative h-[25rem]">
+                <Icon className="absolute h-6 w-6 -top-3 -left-3  text-black" />
+                <Icon className="absolute h-6 w-6 -bottom-3 -left-3  text-black" />
+                <Icon className="absolute h-6 w-6 -top-3 -right-3  text-black" />
+                <Icon className="absolute h-6 w-6 -bottom-3 -right-3  text-black" />
 
                 <EvervaultCard text="Machine Learning" />
 
-                <h2 className="dark:text-white text-black mt-4 text-sm font-light">
+                <h2 className=" text-black mt-4 text-sm font-light">
                     Training models for classification, generation, and detection.
                 </h2>
-                <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
+                <p className="text-sm border font-light border-black/[0.2] rounded-full mt-4 text-black  px-2 py-0.5">
                     2
                 </p>
             </div>
 
-            <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm w-80 mx-auto p-4 relative h-[25rem]">
-                <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
-                <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+            <div className="border border-black/[0.2] flex flex-col items-start max-w-sm w-80 mx-auto p-4 relative h-[25rem]">
+                <Icon className="absolute h-6 w-6 -top-3 -left-3  text-black" />
+                <Icon className="absolute h-6 w-6 -bottom-3 -left-3  text-black" />
+                <Icon className="absolute h-6 w-6 -top-3 -right-3  text-black" />
+                <Icon className="absolute h-6 w-6 -bottom-3 -right-3  text-black" />
 
                 <EvervaultCard text="Systems Programming" />
 
-                <h2 className="dark:text-white text-black mt-4 text-sm font-light">
+                <h2 className=" text-black mt-4 text-sm font-light">
                     Leveraging data structures and algorithms for real world applications.
                 </h2>
-                <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
+                <p className="text-sm border font-light border-black/[0.2] rounded-full mt-4 text-black  px-2 py-0.5">
                     3
                 </p>
             </div>
